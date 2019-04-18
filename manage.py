4b4bd9014a30eb96f6b4ca43d21d3f4e0101478c
‘laowang’ <laowang@163.com>
2019-04-18 11:26:06 +0800

    配置csrf 创建管理类 使用session

--- manage.py
+++ manage.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
+from flask_wtf import CSRFProtect
+from flask_session import Session
+from flask_script import Manager
+
 
 class Config(object):
 
@@ -9,9 +13,18 @@ class Config(object):
     # 不跟踪数据库修改
     SQLALCHEMY_TRACK_MODIFICATIONS =  False
 
-    REDIS_HOST = '17.0.0.1'
+    REDIS_HOST = '127.0.0.1'
     REDIS_PORT = 6379
     REDIS_NUM = 1
+    # 加密字符串
+    SECRET_KEY = "dsadsafasfwqwqdsa21321wqewq"
+    # 调整session存储位置
+    SESSION_TYPE = 'redis'
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT,db=REDIS_NUM)
+    SESSION_USE_SIGNER = True
+    SESSION_PERMANENT = False
+    PERMANENT_SESSION_LIFETIME = 86400*2
+
 
 app = Flask(__name__)
 
@@ -19,11 +32,22 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 
-redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT,db=Config.REDIS_NUM)
+
+# 开启csrf后端保护机制
+csrf = CSRFProtect(app)
+
+# 穿件session拓展类对象
+Session(app)
+
+manager = Manager(app)
 
 @app.route('/')
 def index():
+    session['name'] = 'curry'
     return 'hello world!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    #python manage.py runserver -h   -p    -d
+    manager.run()
